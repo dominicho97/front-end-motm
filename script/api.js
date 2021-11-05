46cf44230e2808c7b3f8439897511e601f56cf08
HoDominic <dominic.ho97@gmail.com>
2021-11-05 11:01:24 +0100

    added leaflet map and marker

--- script/api.js
+++ script/api.js
@@ -2,13 +2,27 @@
 
 
 
-//map
-const mymap = L.map('issMap').setView([51.505, -0.09], 13);
+//make map and tiles
+const mymap = L.map('issMap').setView([0, 0], 1);
+
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>contributors';
+const marker = L.marker([0, 0], { icon: markerIcon }).addTo(mymap);
 
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 tiles = L.tileLayer((tileUrl), { attribution })
 tiles.addTo(mymap)
+
+
+//make marker with custom icon
+var markerIcon = L.icon({
+    iconUrl: 'img/rocket-icon.png',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16],
+
+});
+
+
+
 
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
 
@@ -32,6 +46,9 @@ try {
         //round numbers to 2 decimals
         let roundedLatitude = Math.round((data.latitude + Number.EPSILON) * 100) / 100;
         let roundedLongitude = Math.round((data.longitude + Number.EPSILON) * 100) / 100;
+
+        //L.marker([roundedLatitude, roundedLongitude]).addTo(mymap);
+        marker.setLatLng([roundedLatitude, roundedLongitude])
 
 
         document.getElementById("lat").textContent = roundedLatitude
