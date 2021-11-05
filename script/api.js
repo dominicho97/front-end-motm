7d702fe0fc1ceadb434ac93ab6e82ab58835810b
HoDominic <dominic.ho97@gmail.com>
2021-11-05 10:12:41 +0100

    added Leaflet map in api.js

--- script/api.js
+++ script/api.js
@@ -4,6 +4,11 @@
 
 //map
 const mymap = L.map('issMap').setView([51.505, -0.09], 13);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>contributors';
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+tiles = L.tileLayer((tileUrl), { attribution })
+tiles.addTo(mymap)
 
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
 
